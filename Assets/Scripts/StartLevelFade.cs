0c5f8a96d4a96768dd53994e25d44fd80eec2bbb
Keryann Malacchina <keryannm@gmail.com>
2018-11-23 17:15:37 +0100

    StartLevel Fade Tweak

--- Assets/Scripts/StartLevelFade.cs
+++ Assets/Scripts/StartLevelFade.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
 
-public class StartLevelFade : MonoBehaviour {
+public class StartLevelFade : MonoBehaviour
+{
 
     public CanvasGroup blackFade;
 
-	void Start ()
+    void Start()
     {
+        blackFade.alpha = 1;
+
         blackFade.DOFade(0, 1.5f).OnComplete(() =>
         {
             this.gameObject.SetActive(false);
