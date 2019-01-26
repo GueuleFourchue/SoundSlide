496522616474bf5bb70db9c236b79b73c392c517
Keryann Malacchina <keryannm@gmail.com>
2019-01-26 18:31:55 +0100

    BUILD 1.1.5 : Camera Follow & Quality Settings

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -2,31 +2,34 @@
 using System.Collections;
 using DG.Tweening;
 
-public class CameraFollow : MonoBehaviour {
-	 
-	public Transform cameraSpot;
-	public Transform avatar; 
+public class CameraFollow : MonoBehaviour
+{
+    public Transform cameraSpot;
+    public Transform avatar;
 
-	public float moveLerp = 0.1f;
-	public float rotateLerp = 0.1f;
+    public float moveLerp = 0.1f;
+    public float rotateLerp = 0.1f;
 
-	public float xRotation = 15f;
+    public float xRotation = 15f;
 
-	void Start () 
-	{
-		
-	}
-		
-	void LateUpdate ()
-	{
-		// Z Position
-		transform.position = new Vector3 (transform.position.x, transform.position.y, cameraSpot.transform.position.z);
-			
-		//Position
-		transform.position = Vector3.Lerp (transform.position, cameraSpot.position, moveLerp);
+    void Start()
+    {
 
-		//Rotation
-		transform.rotation = Quaternion.Lerp (transform.rotation, cameraSpot.rotation, rotateLerp);
-		//transform.DOLocalRotate (cameraSpot.eulerAngles, rotateLerp).SetEase(Ease.Linear);
-	}
+    }
+
+    void FixedUpdate()
+    {
+        //Position
+        transform.position = Vector3.Lerp(transform.position, cameraSpot.position, moveLerp);
+
+        //Rotation
+        transform.rotation = Quaternion.Lerp(transform.rotation, cameraSpot.rotation, rotateLerp);
+        //transform.DOLocalRotate (cameraSpot.eulerAngles, rotateLerp).SetEase(Ease.Linear);
+    }
+
+    void LateUpdate()
+    {
+        // Z Position
+        transform.position = new Vector3(transform.position.x, transform.position.y, cameraSpot.transform.position.z);
+    }
 }
